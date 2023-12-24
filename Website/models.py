53c513fb14554b346656b2ec7cd323ce09af062f
Reinder Veldman <reinderv17@gmail.com>
2023-12-24 14:33:52 +0100

    Login done

--- Website/models.py
+++ Website/models.py
@@ -1,22 +1,22 @@
 from . import db
 from flask_login import UserMixin
 
-class Bungalow(db.model):
-    id = db.collumn(db.integer, primary_key=True)
-    naam = db.collumn(db.string(255), unique=True)
-    type = db.collumn(db.integer, db.ForeignKey('bungalowtype.id'))
+class Bungalow(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    naam = db.Column(db.String(255), unique=True)
+    type = db.Column(db.Integer, db.ForeignKey('bungalowtype.id'))
 
     bungalow_type = db.relationship('Bungalowtype', back_populates='bungalows')
 
-class Bungalowtype(db.model):
-    id = db.collumn(db.integer, primary_key=True)
-    aantal_personen = db.collumn(db.integer)
-    weekprijs = db.collumn(db.float)
+class Bungalowtype(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    aantal_personen = db.Column(db.Integer)
+    weekprijs = db.Column(db.Float)
 
     bungalows = db.relationship('Bungalow', back_populates='bungalow_type')
 
-class User(db.model, UserMixin):
-    id = db.Collumn(db.integer, primary_key=True)
-    email = db.collumn(db.string(255), unique=True)
-    password = db.cullumn(db.string(255))
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
 
